cdee91f8f8d7287012c4d2384303c8f23e0e600d
dwillcox <eugene.willcox@gmail.com>
2015-11-06 16:20:18 -0500

    Added a matplotlibrc, got the python scripts working.

--- EvalHSE/InterpolateDatasets.py
+++ EvalHSE/InterpolateDatasets.py
@@ -42,26 +42,46 @@ class InterpolateDatasets:
             print 'No reference dataset.'
             exit()
         else:
-            self.interp_list = [self.interpolate_ds(ds_cf) for ds_cf in self.dataset_list]
+            self.interp_list = [self.interpolate_ds(ds_cf,self.dataset_ref) for ds_cf in self.dataset_list]
             
 
 class ElementwiseStats:
-    def cf_array_stat(stat_fun, array_list):
+    def cf_arrays_op(self,stat_fun, array_list):
         # Takes a list of same-shape numpy arrays and returns the element-wise operation.
         # stat_fun should be a 2-argument numpy element-wise associative comparison function
-        # stat_fun could be, e.g. numpy.minimum, numpy.maximum, etc.n
-        a_max = array_list[0]
+        # stat_fun could be, e.g. numpy.minimum, numpy.maximum, etc. (But NOT, eg. subtract)
+        a_stat = array_list[0] # stat_fun must be a function where this makes sense!!!!!!
         for a in array_list:
-            a_max = stat_fun(a_max,a)
+            a_stat = stat_fun(a_stat,a)
+        return a_stat
         
-    def cf_dicts_stat(stat_fun, dict_list):
+    def cf_dicts_op(self,stat_fun, dict_list):
         # Takes a list of dictionaries dict_list
         # All dictionaries d in dict_list have the same keys pointing to
         ## numpy arrays of the same shape.
         # Returns a single dictionary with the keys of d containing the element-wise op.
         # stat_fun should be a 2-argument numpy element-wise associative comparison function
         # stat_fun could be, e.g. numpy.minimum, numpy.maximum, etc.
-        d_max = {}
+        # It must be possible to initialize stat_fun with one of the list elements.
+        d_stat = {}
         keys = dict_list[0].keys()
         for k in keys:
-            d_max[k] = self.cf_array_stat(stat_fun, [d[k] for d in dict_list])
+            d_stat[k] = self.cf_arrays_op(stat_fun, [d[k] for d in dict_list])
+        return d_stat
+
+    def dicts_subtract(self,A,B):
+        # Given dictionaries A and B, returns the key-wise A-B.
+        d_sub = {}
+        keys = A.keys()
+        for k in keys:
+            d_sub[k] = np.subtract(A[k],B[k])
+        return d_sub
+
+    def dicts_add(self,dict_list):
+        # Given a list of dictionaries, returns their key-wise sum.
+        d_add = dict([(k,np.zeros_like(v)) for k,v in dict_list[0].iteritems()])
+        keys = dict_list[0].keys()
+        for d in dict_list:
+            for k in keys:
+                d_add[k] = np.add(d_add[k],d[k])
+        return d_add
--- EvalHSE/eval_hse_yt.py
+++ EvalHSE/eval_hse_yt.py
@@ -17,60 +17,79 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('refdataset', type=str, help='Name of the reference dataset.')
-parser.add_argument('-cfdataset','--cfdataset', nargs='*', type=str, help='Name(s) of the datasets to be compared to the reference dataset.', required=False)
+parser.add_argument('-cfds','--cfdataset', nargs='*', type=str, help='Name(s) of the datasets to be compared to the reference dataset.', required=False)
 parser.add_argument('-axis','--axis',type=int, help='Axis along which to cast the ray. For Cartesian coordinates, (0=x, 1=y, 2=z). For cylindrical coordinates, (0=r, 1=z, 2=theta). Enter 0, 1, or 2.', required=True)
-parser.add_argument('-coord','--coordinates',type=int,help='Coordinate system to use. (0=Cartesian, 1=Polar)', required=True)
+parser.add_argument('-coord','--coordinates',type=int,help='Coordinate system to use. (0=Cartesian, 1=Cylindrical)', required=True)
 args = parser.parse_args()
 
 if (args.axis != 0 and args.axis != 1 and args.axis != 2):
         print 'Error: --axis argument must be 0, 1, or 2.'
         exit()
         
-crt_lu = {'0':'x','1':'y','2':'z'}
-cyl_lu = {'0':'r','1':'z','2':'theta'}
+dcrt_lu = {'0':'dx','1':'dy','2':'dz'}
+dcyl_lu = {'0':'dr','1':'dz','2':'dtheta'}
         
 if (args.coordinates == 0):
-        interp_axis = crt_lu[str(args.axis)]
+        interp_daxis = dcrt_lu[str(args.axis)]
 elif (args.coordinates == 1):
-        interp_axis = cyl_lu[str(args.axis)]
+        interp_daxis = dcyl_lu[str(args.axis)]
 else:
         print 'Error: --coordinates argument must be 0 or 1.'
         exit()
-
+interp_axis = interp_daxis[1:]
+        
 def get_np_ray(ray_ortho,flist):
-        drays = {}
+        global interp_daxis, interp_axis
+        dray = {}
         for f in flist:
-                drays[f] = np.array(ray_ortho[f])
-        return drays
+                #print f
+                dray[f] = np.array(ray_ortho[f])
+        np_interp_daxis = np.array(ray_ortho[interp_daxis])
+        dray[interp_axis] = np.cumsum(np_interp_daxis)-0.5*np_interp_daxis
+        return dray
         
 ds_ref = yt.load(args.refdataset)
 fields = ds_ref.field_list
-fields.append(interp_axis)
 dray_ref = get_np_ray(ds_ref.ortho_ray(args.axis, (0.0,0.0)),fields)
 dict_ref = {interp_axis:np.array(dray_ref[interp_axis])}
 
-integrated_residuals = {}
-for f in fields:
-        integrated_residuals[f] = 0.0
+################ Setup Plot
+plt.figure()
+fig = plt.gcf()
+ax = fig.add_axes([0.1,0.1,0.8,0.8])
+################ End Setup Plot
+
+integrated_residuals = dict([(f,0.0) for f in fields])
         
 if args.cfdataset:
         dray_cf_list = []
         for cfds in args.cfdataset:
                 ds_cf = yt.load(cfds)
                 dray_cf_list.append(get_np_ray(ds_cf.ortho_ray(args.axis, (0.0,0.0)),
-                                    fields))
+                                               fields))
         interpolator = InterpolateDatasets(dray_cf_list,dict_ref)
         dray_interp_list = interpolator.get_interp_list()
         dray_interp_list.append(dray_ref)
+        #print dray_interp_list
         estats = ElementwiseStats()
-        dray_cf_max = estats.cf_dicts_stat(np.maximum, dray_interp_list)
-        dray_cf_min = estats.cf_dicts_stat(np.maximum, dray_interp_list)
+        dray_cf_max = estats.cf_dicts_op(np.maximum, dray_interp_list)
+        dray_cf_min = estats.cf_dicts_op(np.minimum, dray_interp_list)
+        #print dray_cf_max
+        #print dray_cf_min
 
+        ############# Plot max and min
+        ax.plot(dray_cf_max[interp_axis],dray_cf_max[('flash', u'dens')],color='red',
+                label='max')
+        ax.plot(dray_cf_min[interp_axis],dray_cf_min[('flash', u'dens')],color='green',
+                label='min')
+        ############# End Plot max and min
+        
         # Get the sum of the magnitudes of upper residual and the lower residual
-        dray_residual_upper = estats.cf_dicts_stat(np.subtract, [dray_cf_max, dray_ref])
-        dray_residual_lower = estats.cf_dicts_stat(np.subtract, [dray_ref, dray_cf_min])
-        dray_residual_total = estats.cf_dicts_stat(np.add, [dray_residual_upper,
-                                                           dray_residual_lower])
+        dray_residual_upper = estats.dicts_subtract(dray_cf_max, dray_ref)
+        #print dray_residual_upper
+        dray_residual_lower = estats.dicts_subtract(dray_ref, dray_cf_min)
+        #print dray_residual_lower
+        dray_residual_total = estats.dicts_add([dray_residual_upper, dray_residual_lower])
 
         # Integrate the residual in all fields to estimate the amount of fluctuation
         integrated_residuals = {}
@@ -81,3 +100,13 @@ for k in fields:
         print '-----------------------'
         print k
         print integrated_residuals[k]
+
+################ Plot Reference Data
+ax.plot(dray_ref[interp_axis],dray_ref[('flash', u'dens')],color='blue',label='ref')
+plt.legend()
+plt.xlabel('Log10 ' + interp_axis)
+plt.ylabel('Log10 ' + 'dens')
+ax.set_xscale('log')
+ax.set_yscale('log')
+plt.savefig('dens_max_min.eps')
+################ End Plot Reference Data
