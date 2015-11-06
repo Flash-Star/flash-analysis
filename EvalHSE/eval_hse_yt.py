b0811365a84513fb0eac5bd956aff57dca7fd9f4
dwillcox <eugene.willcox@gmail.com>
2015-11-05 21:53:23 -0500

    Added an interpolation class and some updates.

--- EvalHSE/eval_hse_yt.py
+++ EvalHSE/eval_hse_yt.py
@@ -1,56 +1,58 @@
-from yt.mods import *
+import yt
 import numpy as np
-import pylab as P
-import os
+import matplotlib
+from matplotlib import pyplot as plt
+from InterpolateDatasets import InterpolateDatasets
+from InterpolateDatasets import ElementwiseStats
+import argparse
 
-filePrefix = 'preSNIa_co_wd_m6.3_uniform_Dr-1k_hdf5_plt_cnt_'
-fileSuffix = ['0000','0050','0100','0150','0200','0240']
-fileNames = [filePrefix+fS for fS in fileSuffix]
+########################################################
+### This script reads a flash plot file and returns a
+### lineout of all or a particular field along the axis specified.
+### For a FLASH plt_cnt file with cylindrical (r,z,theta) coordinates:
+### ## Axis 0: radial axis
+### ## Axis 1: z axis (cyl. axis of symmetry)
+### ## Axis 2: theta axis (not useful)
 
-# Lists to hold profiles, labels, and plot specifications.
-profiles = []
-labels = []
+parser = argparse.ArgumentParser()
+parser.add_argument('refdataset', type=str, help='Name of the reference dataset.')
+parser.add_argument('-cfdataset','--cfdataset', nargs='*', type=str, help='Name(s) of the datasets to be compared to the reference dataset.', required=False)
+parser.add_argument('-axis','--axis',type=int, help='Axis along which to cast the ray. For Cartesian coordinates, (0=x, 1=y, 2=z). For cylindrical coordinates, (0=r, 1=z, 2=theta). Enter 0, 1, or 2.', required=True)
+parser.add_argument('-coord','--coordinates',type=int,help='Coordinate system to use. (0=Cartesian, 1=Polar)', required=True)
+args = parser.parse_args()
 
-#pf = load(fileNames)
-# Loop over each dataset in the time-series.
-# Create a data container to hold the whole dataset.
-#ad = pf.h.sphere([0.0,0.0,0.0],(2.5e8,"cm"))
-# Create a 1d profile of density vs. radius.
-#profiles.append(create_profile(ad, ["Radius"],
-#                                   fields=["dens"],n=512,
-#                                   weight_field=None,
-#                                   accumulation=False))
-# Add labels
-#labels.append("t = %.2f" % pf.current_time)
+if (args.axis != 0 and args.axis != 1 and args.axis != 2):
+        print 'Error: --axis argument must be 0, 1, or 2.'
+        exit()
+        
+crt_lu = {'0':'x','1':'y','2':'z'}
+cyl_lu = {'0':'r','1':'z','2':'theta'}
+        
+if (args.coordinates == 0):
+        interp_axis = crt_lu[str(args.axis)]
+elif (args.coordinates == 1):
+        interp_axis = cyl_lu[str(args.axis)]
+else:
+        print 'Error: --coordinates argument must be 0 or 1.'
+        exit()
 
-# Create the profile plot from the list of profiles.
-#plot = ProfilePlot.from_profiles(profiles, labels=labels)
+ds_ref = yt.load(args.refdataset)
+ray_ref = ds_ref.ortho_ray(args.axis, (0.0,0.0))
+dict_ref = {interp_axis:ray_ref[interp_axis]}
 
-# Save the image.
-#plot.save()
+if args.cfdataset:
+        ray_cf_list = []
+        for cfds in args.cfdataset:
+                ds_cf = yt.load(cfds)
+                ray_cf_list.append(ds_cf.ortho_ray(args.axis, (0.0,0.0)))
+        interpolator = InterpolateDatasets(ray_cf_list,dict_ref)
+        ray_interp_list = interpolator.get_interp_list()
+        ray_interp_list.append(ray_ref)
+        estats = ElementwiseStats()
+        ray_cf_max = estats.cf_dicts_stat(np.maximum, ray_interp_list)
+        ray_cf_min = estats.cf_dicts_stat(np.maximum, ray_interp_list)
+        # Get the sum of the magnitudes of upper residual and the lower residual
+        ## TODO
 
-fig,pax = P.subplots(2)
-for fN in fileNames:
-	pf = load(fN)
-	axcut = 0 # take a line cut along the x axis
-	loccut = (0.0,0.0) # place cut ray at origin
-	ray = pf.h.ortho_ray(axcut, loccut)
-	pax[0].plot(ray['x'], ray['Density'],
-		label='t = %.2f' % pf.current_time)
-	pax[1].semilogy(ray['x'], ray['Density'],
-                label='t = %.2f' % pf.current_time)
-pax[0].set_ylabel('Density')
-pax[0].set_xlabel('X')
-pax[0].set_xlim([0,0.5e8])
-pax[0].set_ylim([1e9,4e9])
-#pax[0].legend(loc = 'upper right')
-pax[1].set_ylabel('Density')
-pax[1].set_xlabel('X')
-pax[1].set_xlim([0,1e9])
-pax[1].legend(loc = 'upper right')
-titobj = pax[0].set_title('Density Lineout along x-axis for preSNIa (Dr=1e3, level 11 ref)')
-P.tight_layout()
-titobj.set_y(1.09)
-fig.subplots_adjust(top=0.86) 
-P.savefig("preSNIa_mass-ave_Dr-1e3_R11_xsweep_ts_dens.png")
+
 
