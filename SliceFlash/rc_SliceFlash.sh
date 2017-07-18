d2e48d67ed54832d1d32bc95580598e93c31743b
dwillcox <eugene.willcox@gmail.com>
2017-07-18 15:26:22 -0400

    Make the density contour argument actually used.

--- SliceFlash/rc_SliceFlash.sh
+++ SliceFlash/rc_SliceFlash.sh
@@ -17,7 +17,7 @@ SliceFlash.py $1 -o oflash_logpres.eps -rd=0.0 -ru=1.5e4 -zd=-2.0e4 -zu=1.0e4 -f
 SliceFlash.py $1 -o oflash_composition.eps -rd=0.0 -ru=1.5e4 -zd=-2.0e4 -zu=1.0e4
 
 # Plot the composition with density contour at $2 g/cm**3
-SliceFlash.py $1 -o oflash_composition_dens.eps -rd=0.0 -ru=1.5e4 -zd=-2.0e4 -zu=1.0e4 --densitycontour=1.0e7
+SliceFlash.py $1 -o oflash_composition_dens.eps -rd=0.0 -ru=1.5e4 -zd=-2.0e4 -zu=1.0e4 --densitycontour=$2
 
 # Nice demonstration of density contour. Plot density with a density contour.
-SliceFlash.py $1 -o oflash_dens_contour.eps -rd=0.0 -ru=1.5e4 -zd=-2.0e4 -zu=1.0e4 -field='dens' -cmap='spring' --densitycontour=5.0e7 --densitycontourwidth=0.5
+SliceFlash.py $1 -o oflash_dens_contour.eps -rd=0.0 -ru=1.5e4 -zd=-2.0e4 -zu=1.0e4 -field='dens' -cmap='spring' --densitycontour=$2 --densitycontourwidth=0.5
